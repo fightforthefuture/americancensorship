7ab06e777ddb5925b7067dc3faa2c2820b43fb47
Pau Santesmasses <pau@tumblr-MacBookPro-3c075419f53b.local>
2011-12-12 14:35:13 -0500

    first pass at twitter and Facebook buttons

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 class Post < ActiveRecord::Base
+  include ActionView::Helpers::TextHelper
+  
   validates_presence_of :post, :email, :name
 
   def censored_post
     self.post.gsub(/\w{4,}/) do |match|
       "█" * match.length
     end
+  end
+  
+  def to_tweet
+    truncate(censored_post, :length => 140) + ' ' + short_url
+  end
+  
+  def short_url
+    'http://censr.net/' + id.to_s
   end
   
 end
