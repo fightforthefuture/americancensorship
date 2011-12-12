1f545aaafab84265bf1fb9bdb6c02f495a1ae7bf
Pau Santesmasses <pau@tumblr-MacBookPro-3c075419f53b.local>
2011-12-12 12:08:37 -0500

    redirect to original version of the post after submitting a congress_letter

--- app/controllers/congress_emails_controller.rb
+++ app/controllers/congress_emails_controller.rb
@@ -3,7 +3,7 @@ class CongressEmailsController < ApplicationController
   def create
     @congress_email = CongressEmail.new(params[:congress_email])
     if @congress_email.save
-      redirect_to :back
+      redirect_to post_url(params[:post_id])
     else
       Rails.logger.warn("There was an error saving an email to congress!")
       Rails.logger.info(@congress_email.inspect)
