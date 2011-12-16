4e851a922898fd42a7921dc9c374546d974d9a0f
Joshua Blount <hello@joshuablount.com>
2011-12-16 09:14:33 -0500

    bring call widget back

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,10 @@ AmericancensorshipOrg::Application.routes.draw do
   match "/moz/", :to => 'home#moz'
   
   match "/js",                :to => redirect("/modal/client.js.min")
-  match "/callwidget",        :to => redirect("/modal/congress-closed-form.html")
-  match "/callwidget/",        :to => redirect("/modal/congress-closed-form.html")
-  match "/callwidget/thanks/", :to => redirect("/modal/congress-closed-form-thanks.html")
-  match "/callwidget/thanks", :to => redirect("/modal/congress-closed-form-thanks.html")
+  match "/callwidget",        :to => redirect("/modal/call-form.html")
+  match "/callwidget/",        :to => redirect("/modal/call-form.html")
+  match "/callwidget/thanks/", :to => redirect("/modal/call-form-thanks.html")
+  match "/callwidget/thanks", :to => redirect("/modal/call-form-thanks.html")
   match "/callwidget/next",   :to => redirect("/modal/call-form-done.html")
   match "/callwidget/next/",   :to => redirect("/modal/call-form-done.html")
 end
