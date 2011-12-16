fd99987ef2b535838919ca15b5ac5b148a67f3b8
Phil Nelson <zero@zerolives.org>
2011-12-16 15:03:24 -0800

    Aliases for forms and JS to aid in switching modes.

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,13 @@ AmericancensorshipOrg::Application.routes.draw do
   match "/callwidget/thanks", :to => redirect("/modal/call-form-thanks.html")
   match "/callwidget/next",   :to => redirect("/modal/call-form-done.html")
   match "/callwidget/next/",   :to => redirect("/modal/call-form-done.html")
+  match "/act-now",							:to => redirect("/modal/call-form.html")
+  match "/act-now-thanks",							:to => redirect("/modal/call-form-thanks.html")
+  match "/act-now/",							:to => redirect("/modal/call-form.html")
+  match "/act-now-thanks/",							:to => redirect("/modal/call-form-thanks.html")
+  match "/act-now-ac",					:to => redirect("/ac2/frames/call-form.html")
+  match "/act-now-ac-thanks",					:to => redirect("/ac2/frames/call-form-thanks.html")
+  match "/act-now-ac/",					:to => redirect("/ac2/frames/call-form.html")
+  match "/act-now-ac-thanks/",					:to => redirect("/ac2/frames/call-form-thanks.html")
+  match "/call-client",					:to => redirect("/modal/call-client.js")
 end
