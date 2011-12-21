31b373591a761ee417c8b991138fa7420398bd4e
Christian Bryan <christianbryan@gmail.com>
2011-12-21 16:21:14 -0500

    Log geo ip responses when possible.

--- public/geo.js
+++ public/geo.js
@@ -4,7 +4,9 @@
  */
 $(function(){
   // Detect non-US users and ask them to petition the state department instead.
-  $.get('http://api.hostip.info/country.php', function(response) {
+  $.get('http://api.hostip.info/country.php?ip=192.168.117.2', function(response) {
+    window.console && console.log && console.log("FFTF Geo response", response);
+
     // If the API doesn't know where the IP is, don't do any magic.
     if(response == 'XX') {
       return;
