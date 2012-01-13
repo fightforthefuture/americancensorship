ce6fbd1f67db9a3dcd1701a06af359aad75681a6
Joshua Blount <hello@joshuablount.com>
2012-01-13 10:31:48 -0500

    fix js to work for new overlay

--- public/modal/sopastrike.client.js
+++ public/modal/sopastrike.client.js
@@ -35,7 +35,7 @@ displayAmericanCensorshipModal = function() {
   // Inject CSS
   if (!global_displayAmericanCensorshipModal) {
       global_displayAmericanCensorshipModal = 1;
-      var css = '#signupmodal-overlay{display:block;position:absolute;top:0;left:0;width:100%;height:100%;background-color:#444;z-index:1001;-moz-opacity:.8;opacity:.8;filter:alpha(opacity=80);}#signupmodal-lightbox{display:block;position:absolute;left:15%;top:15%;width:862px;height:527px;padding:0;margin:0;background-color:#fff;z-index:1500;overflow:hidden;box-shadow:0px 0px 25px #171717;-webkit-border-radius: 3px;-moz-border-radius: 3px;border-radius: 3px;overflow:hidden;}#signupmodal-lightbox iframe{border:0;width:862px;height:527px;overflow:hidden;}#signupmodal-close{color:white;font-family:"Helvetica","Arial",sans-serif;float:right;vertical-align:10px;z-index:100;position:absolute;margin-left:800px;vertical-align:50px;}';
+      var css = '#signupmodal-overlay{display:block;position:absolute;top:0;left:0;width:100%;height:100%;background-color:#444;z-index:1001;-moz-opacity:.8;opacity:.8;filter:alpha(opacity=80);}#signupmodal-lightbox{display:block;position:absolute;left:15%;top:15%;width:1100px;height:800px;padding:0;margin:0;background-color:#fff;z-index:1500;overflow:hidden;box-shadow:0px 0px 25px #171717;-webkit-border-radius: 3px;-moz-border-radius: 3px;border-radius: 3px;overflow:hidden;}#signupmodal-lightbox iframe{border:0;width:1100px;height:800px;overflow:hidden;}#signupmodal-close{color:white;font-family:"Helvetica","Arial",sans-serif;float:right;vertical-align:10px;z-index:100;position:absolute;margin-left:1040px;vertical-align:50px;}';
       var style = document.createElement('style');
       style.type = "text/css";
       style.innerHTML = css;
@@ -47,7 +47,7 @@ displayAmericanCensorshipModal = function() {
                + '<a id="signupmodal-close" href="javascript:void(0)" onclick="javascript:document.getElementById(\'signupmodal-overlay\').style.display=\'none\';document.getElementById(\'signupmodal-lightbox\').style.display=\'none\';">'
                + '<img src="http://americancensorship.org/modal/images/close.png" title="Close"/>'
                + '</a>'
-               + '<iframe src="http://joshuablount.com"></iframe>'
+               + '<iframe src="http://americancensorship.org/modal/sopastrikeoverlay.html"></iframe>'
                + '</div>';
 
       var injector = document.createElement('div');
