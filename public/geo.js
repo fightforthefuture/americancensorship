db61f120ec14c976da317464a1bc37e391bfe422
Joshua Blount <hello@joshuablount.com>
2013-01-17 10:46:23 -0500

    rack app + public files = awesome

---
+++ public/geo.js
@@ -0,0 +1,18 @@
+/* Geolocation Toolkit for http://americancensorship.org
+ * Depends on jQuery
+ * Include code: <script type="text/javascript" src="/geo.js"></script>
+ */
+// $(function(){
+//   // Detect non-US users and ask them to petition the state department instead.
+//   $.get('http://api.hostip.info/country.php', function(response) {
+//     window.console && console.log && console.log("FFTF Geo response", response);
+
+//     // If the API doesn't know where the IP is, don't do any magic.
+//     if(response == 'XX') {
+//       return;
+//     }
+//     if(response !== 'US') {
+//       document.location.pathname = "/modal/state-dept-petition/index.html";
+//     }
+//   });
+// });
